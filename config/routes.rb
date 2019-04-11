3cac2b72e7c1a0a7855c5c7c36d43cf849c25ec7
Mayra Navarro <luciagirasoles@gmail.com>
2019-04-11 17:58:28 -0400

    add routes (#4)
    * add routes
    
    * Modify routes.rb
    
    * update routes.rb
    
    * remove to update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :songs, only: [:index, :show] do
+      get ':resource', to: 'songs#index'
+      patch 'progress', to: 'songs#update'
+      get 'search', on: :collection
+    end
+    resources :artists, only: [:index, :show] do
+      get ':resource', to: 'artists#index'
+      get 'search', on: :collection
+    end
+    resources :albums, only: [:index,:show]  do
+      get ':resource', to: 'albums#index'
+      get 'search', on: :collection
+    end
+  end
 end
