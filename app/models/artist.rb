c5aaa02323b13c7ac7fdad9c42efa7624cf678d9
Lian Nivin <38519819+liannivin@users.noreply.github.com>
2019-04-13 21:06:30 -0500

    Add validation (#23)
    * Configure Gemfile
    
    * Validation test
    
    * Add validations
    
    * Delete controllers
    
    * validation added
    
    * Add comment
    
    * Fix validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,11 @@
 class Artist < ApplicationRecord
   has_and_belongs_to_many :songs, join_table: 'associations', dependent: :delete_all
   has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
-  validates :name, presence: true, length: {minimum: 1, maximum: 50}
+  validates :name, presence: true
   validates :age, presence: false
 
   def self.search(search_name)
     self.where("name LIKE ?", "%#{search_name}%")
   end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,17 @@ class Song < ApplicationRecord
   has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
   has_and_belongs_to_many :artists, join_table: 'associations', dependent: :delete_all
 
-  validates :title, presence: true, length: {minimum: 1, maximum: 50}
+  validates :title, presence: true
   validates :duration, presence: false
   validates :rating, presence: false
   validates :progress, presence: false
+  validates :progress_must_less_than_duration
+  
+  # Requirement: The progress could not be bigger than the duration.
+  def progress_must_less_than_duration
+    if (self.progress > self.duration)
+      errors.add(:progress, "progress must less than duration")
+    end
+  end
+
 end
