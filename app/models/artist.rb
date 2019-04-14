58713419f49f6841da00ae21f00d06f44283934e
Mayra Navarro <luciagirasoles@gmail.com>
2019-04-13 21:03:43 -0400

    Test artist controller (#21)
    * finish all GET index tests
    
    * Add tests and controllers for GET show
    
    * fix end of class Artists_Controller
    
    * Add  TDD GET search for API Artist
    
    * Fix indentation line 22
    
    * Update artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,8 @@ class Artist < ApplicationRecord
   has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
   validates :name, presence: true, length: {minimum: 1, maximum: 50}
   validates :age, presence: false
+
+  def self.search(search_name)
+    self.where("name LIKE ?", "%#{search_name}%")
+  end
 end
--- spec/controllers/api/artists_controller_spec.rb
+++ spec/controllers/api/artists_controller_spec.rb
@@ -1,17 +1,62 @@
 require 'rails_helper'
+
+def createArtist
+  Artist.create(name: "Nujabes", age: "31")
+end
+
 describe Api::ArtistsController do
   before do
-    @artist = Artist.create(name: "Nujabes", age: "31")
+    Artist.delete_all
   end
-
-  describe 'GET' do
+  describe 'GET index' do
     it 'returns http status ok' do
       get :index
       expect(response).to have_http_status(:ok)
     end
     it 'returns http status ok when get one movie data' do
-      get :index, params: {id: @artist.id}
+      artist = createArtist
+      get :index, params: {id: artist.id}
       expect(response).to have_http_status(:ok)
+    end
+    it 'render json with all artists' do
+      createArtist
+      get :index
+      artists = JSON.parse(response.body)
+      expect(artists.size).to eq 1
+    end
+  end
+  describe 'GET show' do
+    it 'returns http status ok' do
+      artist = createArtist
+      get :show, params: { id: artist.id }
+      expect(response).to have_http_status(:ok)
+    end
+    it 'render the correct user' do
+      artist = createArtist
+      get :show, params: { id: artist.id }
+      expected_artist = JSON.parse(response.body)
+      expect(expected_artist["id"]).to eq(artist.id)
+    end
+    it 'returns http status not found' do
+      get :show, params: { id: 'x'}
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+  describe 'GET search' do
+    it 'returns http status ok' do
+      artist = createArtist
+      get :search, params: { search: artist.name }
+      expect(response).to have_http_status(:ok)
+    end
+    it 'returns list related to the search' do
+      createArtist 
+      get :search, params: {search: 'uj'}
+      artists = JSON.parse(response.body)
+      expect(artists.count).to eq 1
+    end
+    it 'returns http status not found' do
+      get :search, params: { search: 'xxx' }
+      expect(response).to have_http_status(:not_found)
     end
   end
 
